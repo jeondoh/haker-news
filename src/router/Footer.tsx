4ae410faf690b728c47d3b25ff5ef898dd2a4e02
jundoh <jundo1302@gmail.com>
2022-01-30 00:26:21 +0900

    feat: footer done

--- src/router/Footer.tsx
+++ src/router/Footer.tsx
@@ -1,5 +1,36 @@
-import { Wrapper } from "../styles/FooterStyle";
+import {
+  Img,
+  Img_Text,
+  Wrapper,
+  Wrapper_Flex,
+  Wrapper_Img,
+} from "../styles/FooterStyle";
 
 export default function Footer() {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <Wrapper_Flex>
+        <Wrapper_Img>
+          <Img src={require("../images/top.png")} alt="Logo" />
+          <Img_Text>Top</Img_Text>
+        </Wrapper_Img>
+        <Wrapper_Img>
+          <Img src={require("../images/new.png")} alt="Logo" />
+          <Img_Text>New</Img_Text>
+        </Wrapper_Img>
+        <Wrapper_Img>
+          <Img src={require("../images/ask.png")} alt="Logo" />
+          <Img_Text>Ask</Img_Text>
+        </Wrapper_Img>
+        <Wrapper_Img>
+          <Img src={require("../images/show.png")} alt="Logo" />
+          <Img_Text>Show</Img_Text>
+        </Wrapper_Img>
+        <Wrapper_Img>
+          <Img src={require("../images/jobs.png")} alt="Logo" />
+          <Img_Text>Jobs</Img_Text>
+        </Wrapper_Img>
+      </Wrapper_Flex>
+    </Wrapper>
+  );
 }
--- src/styles/FooterStyle.ts
+++ src/styles/FooterStyle.ts
@@ -2,10 +2,36 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   position: relative;
+  padding: 0 10px;
   width: 100%;
   height: 83px;
   bottom: 0;
   border-radius: 0 0 50px 50px;
   overflow: hidden;
-  border-top: 1px solid #fd6106;
+  border-top: 1px solid #808080;
+`;
+export const Wrapper_Flex = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 51px;
+`;
+export const Wrapper_Img = styled.div`
+  cursor: pointer;
+`;
+
+/* 하단바 이미지 */
+export const Img = styled.img`
+  display: block;
+  width: 32px;
+  height: 32px;
+`;
+/* 하단바 이미지 글자 */
+export const Img_Text = styled.p`
+  width: 31px;
+  text-align: center;
+  vertical-align: bottom;
+  font-size: 8.5px;
+  line-height: 10px;
+  color: ${(props) => props.theme.textColor};
 `;
