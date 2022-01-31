461b338e0d1a74c3c82b4372a81b570f40b0019d
jundoh <jundo1302@gmail.com>
2022-02-01 00:06:29 +0900

    fix: react-query add option
    refetchOnWindowFocus: false

--- src/utils/callApi.ts
+++ src/utils/callApi.ts
@@ -7,27 +7,33 @@ export function getCategoryIds(category: string) {
   return fetch(`${BASE_PATH}/${category}.json`).then((res) => res.json());
 }
 /* 카테고리별 API */
-export function GetAPIDatas() {
+export function GetAPIData() {
   return useQueries([
     {
       queryKey: ["getTopId", "top-stories"],
       queryFn: () => getCategoryIds("topstories"),
+      // 윈도우를 포커스 했을때 refetch 되지 않음. (default : true)
+      refetchOnWindowFocus: false,
     },
     {
       queryKey: ["getNewId", "new-stories"],
       queryFn: () => getCategoryIds("newstories"),
+      refetchOnWindowFocus: false,
     },
     {
       queryKey: ["getAskId", "ask-stories"],
       queryFn: () => getCategoryIds("askstories"),
+      refetchOnWindowFocus: false,
     },
     {
       queryKey: ["getShowId", "show-stories"],
       queryFn: () => getCategoryIds("showstories"),
+      refetchOnWindowFocus: false,
     },
     {
       queryKey: ["getJobId", "job-stories"],
       queryFn: () => getCategoryIds("jobstories"),
+      refetchOnWindowFocus: false,
     },
   ]);
 }
