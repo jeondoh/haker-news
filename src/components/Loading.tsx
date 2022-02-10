58fabd11178382bec95fb97851ca6384057eb9b7
jundoh <jundo1302@gmail.com>
2022-02-10 16:41:39 +0900

    feat: add Loading dot animation

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,74 @@
+import { motion } from "framer-motion";
 import { LoadingDiv } from "../styles/LoadingStyle";
 
+const loadingContainerVariants = {
+  start: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+  end: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const loadingContainer = {
+  width: "2rem",
+  height: "2rem",
+  display: "flex",
+  justifyContent: "space-around",
+};
+
+const loadingCircle = {
+  display: "block",
+  width: "0.5rem",
+  height: "0.5rem",
+  backgroundColor: "black",
+  borderRadius: "0.25rem",
+};
+
+const loadingCircleVariants = {
+  start: {
+    y: "0%",
+  },
+  end: {
+    y: "100%",
+  },
+};
+
+const loadingCircleTransition = {
+  duration: 0.6,
+  yoyo: Infinity,
+  ease: "easeInOut",
+};
+
 export default function Loading() {
-  return <LoadingDiv>Loading</LoadingDiv>;
+  return (
+    <LoadingDiv>
+      <motion.div
+        style={loadingContainer}
+        variants={loadingContainerVariants}
+        initial="start"
+        animate="end"
+      >
+        <motion.span
+          style={loadingCircle}
+          variants={loadingCircleVariants}
+          transition={loadingCircleTransition}
+        />
+        <motion.span
+          style={loadingCircle}
+          variants={loadingCircleVariants}
+          transition={loadingCircleTransition}
+        />
+        <motion.span
+          style={loadingCircle}
+          variants={loadingCircleVariants}
+          transition={loadingCircleTransition}
+        />
+      </motion.div>
+    </LoadingDiv>
+  );
 }
